8ef768e147a48e12b0292de13d7e700d2ce9d074
09877a8954a98ae1b50358718b00af56c99e4229 <janpieter.kunst@di.huc.knaw.nl>
2023-09-12 15:42:43 +0200

    find correct autoload.php

--- test/testconfig_local.inc.php
+++ test/testconfig_local.inc.php
@@ -1,6 +1,10 @@
 <?php
 
-require('../vendor/autoload.php');
+if (file_exists('../vendor/autoload.php')) {
+    require('../vendor/autoload.php');
+} else {
+    require('../../../autoload.php');
+}
 define('KAART_TESTDIRECTORY', realpath('./tmp/'));
 #define('KAART_SERVER_HOSTNAME', 'localhost');
 #define('KAART_SERVER_PATH', '/');
